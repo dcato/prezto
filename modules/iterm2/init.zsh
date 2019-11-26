75288695f92e3d764d37bf91f6202bccc8f640e2
Daisuke Cato <daisuke.cato@gmail.com>
2019-11-27 00:16:55 +0900

    initial implementation of iTerm2 module.

---
+++ modules/iterm2/init.zsh
@@ -0,0 +1,15 @@
+#
+# Defines iTerm2 functions and aliases
+#
+# Authors:
+#   Daiuske Cato <daisuke.cato@gmail.com>
+#
+
+# Return if requirements are not found.
+if [[ "$LC_TERMINAL" != "iTerm2" ]]; then
+  return 1
+fi
+
+#
+# Aliases
+#
